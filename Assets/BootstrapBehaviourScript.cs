4cd9bd02cd3f48f3327701f112f98cf5884f0c76
reuke <chuzovp@gmail.com>
2022-07-22 20:13:12 +0700

    Adjust camera position to always fit to cards

--- Assets/BootstrapBehaviourScript.cs
+++ Assets/BootstrapBehaviourScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class BootstrapBehaviourScript : MonoBehaviour
 {
     const int cardsCount = 5;
-    const float cardsSpacing = 5;
+    const float cardsSpacingMultiplier = 5;
 
     private List<Card> cards = new List<Card>(cardsCount);
     private Button loadButton;
@@ -18,7 +18,7 @@ public class BootstrapBehaviourScript : MonoBehaviour
         for (int i = 0; i < cardsCount; i++)
         {
             var card = new Card();
-            card.Position = new Vector3((i - cardsCount / 2.0f + 0.5f) * Card.Width * cardsSpacing, 0.0f, 0.0f);
+            card.Position = new Vector3((i - cardsCount / 2.0f + 0.5f) * Card.Width * cardsSpacingMultiplier, 0.0f, 0.0f);
             cards.Add(card);
         }
 
@@ -33,11 +33,36 @@ public class BootstrapBehaviourScript : MonoBehaviour
 
     void Start()
     {
+        StartCoroutine(OnAspectChangedCoroutine());
     }
 
     void Update()
     {
 
+    }
+
+    private float cameraAspect = 0.0f;
+
+    private IEnumerator OnAspectChangedCoroutine()
+    {
+        while (true)
+        {
+            if (Camera.main.aspect != cameraAspect)
+            {
+                AdjustCameraToFitCards();
+                cameraAspect = Camera.main.aspect;
+            }
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+
+    static void AdjustCameraToFitCards()
+    {
+        var frustumWidth = (cardsCount + 1) * Card.Width * cardsSpacingMultiplier;
+        var frustumHeight = frustumWidth / Camera.main.aspect;
+        var distance = frustumHeight * 0.5f / Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        Camera.main.transform.position = new Vector3(0.0f, 0.0f, distance * -1);
+        Camera.main.nearClipPlane = distance - Card.Width * 2;
     }
 
     public void OnLoadButtonClick()
