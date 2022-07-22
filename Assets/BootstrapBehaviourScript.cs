3efcfbace6e9fad31f3c5839531de3e41820b24d
reuke <chuzovp@gmail.com>
2022-07-22 19:15:36 +0700

    Empty UI

--- Assets/BootstrapBehaviourScript.cs
+++ Assets/BootstrapBehaviourScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BootstrapBehaviourScript : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class BootstrapBehaviourScript : MonoBehaviour
     const float cardsSpacing = 5;
 
     private List<Card> cards = new List<Card>(cardsCount);
+    private Button loadButton;
+    private Button stopButton;
+    private Dropdown modeSelectionDropdown;
 
     void Awake()
     {
@@ -17,15 +21,41 @@ public class BootstrapBehaviourScript : MonoBehaviour
             card.Position = new Vector3((i - cardsCount / 2.0f + 0.5f) * Card.Width * cardsSpacing, 0.0f, 0.0f);
             cards.Add(card);
         }
+
+        loadButton = GameObject.Find("LoadButton").GetComponent<Button>();
+        loadButton.onClick.AddListener(OnLoadButtonClick);
+
+        stopButton = GameObject.Find("StopButton").GetComponent<Button>();
+        stopButton.onClick.AddListener(OnStopButtonClick);
+
+        modeSelectionDropdown = GameObject.Find("ModeSelectionDropdown").GetComponent<Dropdown>();
     }
 
     void Start()
     {
-
     }
 
     void Update()
     {
 
     }
+
+    public void OnLoadButtonClick()
+    {
+        Debug.Log("OnLoadButtonClick");
+        var selectedMode = (LoadingMode)modeSelectionDropdown.value;
+        Debug.Log(selectedMode);
+    }
+
+    public void OnStopButtonClick()
+    {
+        Debug.Log("OnStopButtonClick");
+    }
+}
+
+public enum LoadingMode
+{
+    AllAtOnce = 0,
+    OneByOne = 1,
+    WhenImageReady = 2,
 }
